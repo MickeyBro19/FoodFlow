4e4826026fa64f3f0a43bd36b25f47b2b6c5dd14
Mickey <bhaskarp986@gmail.com>
2026-08-22 00:06:41 +0530

    fix: fetch food item relationships efficiently

--- src/main/java/com/mickey/foodflow/repository/FoodItemRepository.java
+++ src/main/java/com/mickey/foodflow/repository/FoodItemRepository.java
@@ -1,13 +1,41 @@
 package com.mickey.foodflow.repository;
 
-import com.mickey.foodflow.entity.Category;
 import com.mickey.foodflow.entity.FoodItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FoodItemRepository extends JpaRepository<FoodItem,Long> {
-    List<FoodItem> findByRestaurantRating(Long restaurantId);
+    @Query("""
+    SELECT f from FoodItem f
+    join fetch  f.category
+    join fetch f.restaurant
+""")
+    List<FoodItem> findAllWithCategoryAndRestaurant();
 
-    List<FoodItem> findByCategory(Long categoryId);
+    @Query("""
+    select f from FoodItem f
+    join fetch f.category
+    join fetch f.restaurant
+    where f.restaurant.id= :restaurantId
+""")
+    List<FoodItem> findByRestaurantIdWithDetails(Long restaurantId);
+
+    @Query("""
+    select f from FoodItem f
+    join fetch f.category
+    join fetch f.restaurant
+    where f.category.id= :categoryId
+""")
+    List<FoodItem> findByCategoryIdWithDetails(Long categoryId);
+
+    @Query("""
+    select f from FoodItem f
+    join fetch f.category
+    join fetch f.restaurant
+    where f.id=:foodId
+""")
+    Optional<FoodItem> findByIdWithDetails(Long foodId);
 }
